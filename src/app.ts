f49217951f9318cb7a70e41e3ddbc9885f063e51
tomibudis <tomibudis@gmail.com>
2024-04-04 21:29:12 +0700

    feat: enable cors

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ require('dotenv').config()
 
 import 'dotenv/config';
 import express from "express";
+import cors from 'cors';
 import api from './routes/api.routes';
 import {swaggerDocs} from './config/swagger';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get<Record<string, unknown>, { message: string }>('/', (_, res) => {
   res.json({
