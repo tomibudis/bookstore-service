480ee6fbc88cdb033c48e993edee779618880b07
tomibudis <tomibudis@gmail.com>
2024-04-04 21:44:37 +0700

    fix: cors config

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import {swaggerDocs} from './config/swagger';
 const app = express();
 const corsOptions = {
   credentials: true,
-  origin: ['http://localhost:3000', 'http://localhost:80']
+  origin: "*"
 }
 app.use(cors(corsOptions));
 
